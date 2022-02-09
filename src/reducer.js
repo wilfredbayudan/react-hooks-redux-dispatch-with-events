cd198ed1d2bb997a415e3ac5c85ac80dbac2ebad
Wilfred <wilfredbayudan@gmail.com>
2022-02-08 14:15:09 -1000

    commit tree a59c233820337ba783d05fc3e64e5383c6add334

--- src/reducer.js
+++ src/reducer.js
@@ -1,1 +1,26 @@
 // add code snippets from README
+let state;
+
+function reducer(state = { count: 0 }, action) {
+  switch (action.type) {
+    case "counter/increment":
+      return { count: state.count + 1 };
+    default:
+      return state;
+  }
+}
+
+function dispatch(action) {
+  state = reducer(state, action)
+  render();
+}
+
+function render() {
+  const container = document.querySelector("#container");
+  container.textContent = state.count;
+}
+
+dispatch({ type: '@@INIT '});
+
+const button = document.querySelector("#button");
+button.addEventListener('click', () => dispatch({ type: "counter/increment"} ));
